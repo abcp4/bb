6204df95e6ae557426a8bf3facfc265a6282571b
Ivan Pereira <navi1921@gmail.com>
2019-07-03 18:41:08 -0300

    Add files via upload

--- bb.py
+++ bb.py
@@ -1,32 +1,28 @@
 import numpy as np
+import load
 #queremos maximizar:
-def f(x,S,c):
+def f(x,S,c,m):
 	#aplica-se a mascara sobre as variaveis
 	val = 0
 	for i in range(m):
-		S1 = S[i]*x
-		val+= c[i]*S1
+		# a mascara tem que ir só até o tamanho dos valores
+		mult = 1
+		print(S[i])
+		for j in range(len(S[i])):
+			if(x[j]==1):
+				print('S[i][j]:',S[i][j])
+				mult*=S[i][j]
+		print(mult)
+		print(c[i])
+		val+= c[i]*mult
+		print(val)
+		a = 2/0
 	return val
 
-#produtos sao da forma c*m
-m = 5 #numero de produtos
-c = []#coeficientes dos produtos
-
-#variaveis
-S = []
-for i in range(m):
-	#esses valores recebemos nas instancias
-	#preenchendo com valores quaisquer...
-    S.append([4,10,5,1])#S1,..,Sm
-    c.append(1) #coef para cada item
-
-
-S = np.asarray(S)
-#coeficientes
-c = np.asarray(c)
-
+S,c = load.loadFile("nl01-40.txt")
+m = len(S)
+print('m: ',m)
 #A mascara boolena que queremos encontrar
-x = np.asarray([0,0,0,1])
-
-v = f(x,S,c)
+x = np.asarray([1,1,1,1])
+v = f(x,S,c,m)
 print(v)
