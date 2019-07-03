fbea5f75472b72b53a071f24ddf470db29d4b356
Ivan Pereira <navi1921@gmail.com>
2019-07-03 19:52:20 -0300

    Update bb.py

--- bb.py
+++ bb.py
@@ -2,23 +2,18 @@ import numpy as np
 import load
 #queremos maximizar:
 def f(x,S,c,m):
-	#aplica-se a mascara sobre as variaveis
 	val = 0
-	for i in range(m):
-		# a mascara tem que ir só até o tamanho dos valores
-		mult = 1
-		#print(S[i])
-		for j in range(len(S[i])):
-			if(x[j]==1):
-				mult*=S[i][j]
-		val+= c[i]*mult
-		#print(val)
+	for i in range(len(x)):
+		if(x[i]==1):
+			val+= c[i]
+			#print(val)
 	return val
 
 S,c = load.loadFile("nl01-40.txt")
 m = len(S)
 print('m: ',m)
+print('S')
 #A mascara boolena que queremos encontrar
-x = np.asarray([1,1,1,1])
+x = np.asarray([0,0,0,0,0,1,0,1])
 v = f(x,S,c,m)
 print(v)
