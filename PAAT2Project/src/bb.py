629299bd68c42d399052b1688159f049b2feee11
Clauvin <clauvin_almeida@hotmail.com>
2019-07-05 16:14:32 -0300

    Making bb function to avoid unnecesary code exceution

--- PAAT2Project/src/bb.py
+++ PAAT2Project/src/bb.py
@@ -9,14 +9,15 @@ def f(x,S,c,m):
             #print(val)
     return val
 
-S,c = load.loadFile("../inputs/nl01-40.txt")
-m = len(S)
-print('m: ',m)
-print('S')
-#A mascara boolena que queremos encontrar
-#6,8
-x = np.zeros(30)
-x[5] = 1
-x[7] = 1
-v = f(x,S,c,m)
-print(v)
+def test():
+    S,c = load.loadFile("../inputs/nl01-40.txt")
+    m = len(S)
+    print('m: ',m)
+    print('S')
+    #A mascara boolena que queremos encontrar
+    #6,8
+    x = np.zeros(30)
+    x[3] = 1
+    x[11] = 1
+    v = f(x,S,c,m)
+    print(v)
