d0fa03a9a154faab93f56bbb86ee2c2f6015c028
Clauvin <clauvin_almeida@hotmail.com>
2019-07-05 16:17:55 -0300

    TypeError bug still unfixed

--- PAAT2Project/src/case1.py
+++ PAAT2Project/src/case1.py
@@ -1,12 +1,14 @@
 import numpy as np
+import bb
 import load
 
 #passo 1: ler dados do arquivo a ser testado
 #passo 2: aplicar branch and bound
 #passo 3: retornar resultado
 
+print("WHAT")
 x_sets,coeficientes = load.loadFile("../inputs/nl01-40.txt")
-x_size = 30 #precisamos que load.loadFile também retorne quantos x existem
+x_size = 30 #precisamos que load.loadFile tambem retorne quantos x existem
 
 length = len(x_sets)
 
@@ -20,25 +22,29 @@ def branch_and_bound():
     x = 0
     last_x = length-1
     
-    while(x <= last_x) || (len(array_de_nos) > 0):
+
+    while(x <= last_x) or (len(array_de_nos) > 0):
         novo_set = []
-        for (set in array_de_nos):
+        for set in array_de_nos:
             set_with = set
             set[x] = 1
             novo_set.append(set_with)
             set_without = set
             novo_set.append(set_without)
         array_de_nos = novo_set
-        for (set in array_de_nos):
-            max_value = max(max_value, f(set, x_sets,coeficientes, length))
+        for set in array_de_nos:
+            f_result = bb.f(set, x_sets, coeficientes, length)
+            gambiarra_f_result = float(f_result)
+            print (f_result)
+            max_value = max(max_value, gambiarra_f_result)
         length_set = len(array_de_nos)
         i = 0
-        #dá pra melhorar essa parte para não ter que fazer DOIS cálculos seguindos
+        #da pra melhorar essa parte para nao ter que fazer DOIS calculos seguindos
         while (i < len(array_de_nos)):
             if (f(array_de_nos[i],x_sets,coeficientes,length) < max_value):
                 array_de_nos.remove(array_de_nos[i])
-            else i++
-        
+            else: i = i+1
+        x = x+1
         
     
     return array_de_nos[0], f(array_de_nos_0, x_sets,coeficientes,length)
@@ -48,7 +54,7 @@ def create_initial_x_set(n):
     return x_set 
 
 def relax_function(x_sets,coeficientes):
-    #maximo valor entre primeiro conjunto disponível e 0
+    #maximo valor entre primeiro conjunto disponivel e 0
     first_coeficient_of_sets_available = 0
     for i in range(len(x_sets)):
         if (x_sets[i][0] == 1):
@@ -56,3 +62,4 @@ def relax_function(x_sets,coeficientes):
             break
     return max(0, first_coeficient_of_sets_available)
     
+branch_and_bound()
